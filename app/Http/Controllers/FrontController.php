0ee928a26f8ab93cc2f4ed452dcf31bc6235dd83
ditha23 <pradithadavina@gmail.com>
2022-06-23 11:49:48 +0700

    first commit

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories\Products;
+
+class FrontController extends Controller
+{
+    public static function getIndex()
+    {
+        $data['products'] = Products::getAll();
+        
+
+        return view('front.index',$data);
+    }
+}
---
+++ resources/views/template/front/head.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>USM Shop</title>
+    <Link rel="icon" type="image/x-icon" href="{{asset('assets/favicon.ico')}}" />
+    <Link href="https://cdn.jsdeLivr.net/npm/bootstrap-icons@1.5.0/front/bootstrap-icons.css" rel="stylesheet" />
+    <Link href="{{asset('assets/css/styles.css')}}" rel="stylesheet" />
+</head>
+<body>
